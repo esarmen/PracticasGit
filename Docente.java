1c2d17639a338af68910da648037fc08e0fbb8fe
esarmen <esm.00@hotmail.com>
2022-03-06 19:34:41 +0100

    Actualizada la clase Docente

--- Docente.java
+++ Docente.java
@@ -1,15 +1,14 @@
-    public class Docente {
+ public class Docente {
         private int horasClase;
         private String nombre;
-        private String apellidos;
-    }
-	
-	public Docente(){
+
+        public Docente(){
 
         }
 
-        public Docente(int horasClase, String nombre, String apellidos) {
+        public Docente(int horasClase, String nombre) {
             this.horasClase = horasClase;
             this.nombre = nombre;
-            this.apellidos = apellidos;
         }
+    } 
+
